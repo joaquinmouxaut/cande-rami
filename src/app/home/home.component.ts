307b56e8101f840c1a8fc33a2fa8b54c3c4241f7
Joaquin <joaquin@techforb.com>
2022-10-06 20:45:58 -0300

    adaptacion a desktop

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +9,12 @@ export class HomeComponent implements OnInit {
   lat = -31.267637195891393;
   lng = -64.33131442649334;
   zoom = 8;
+
+  width: number = window.innerWidth;
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.width = window.innerWidth;
+  }
 
   constructor() { }
 
